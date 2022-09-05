74848b550a39622e77b90d67d631748b073b39ff
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-09-05 19:39:00 -0300

    fixing error heroku

--- api/database/database.js
+++ api/database/database.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
-const connection = new Sequelize('gopresence','postgres','147258', {
-  host: 'localhost',
-  dialect: 'postgres'
-})
-// const connection = new Sequelize(process.env.DATABASE_URL, {
-//     dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false
-//         }
-//     }
+// const connection = new Sequelize('gopresence','postgres','147258', {
+//   host: 'localhost',
+//   dialect: 'postgres'
 // })
+const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
+})
 
 
 module.exports = connection
