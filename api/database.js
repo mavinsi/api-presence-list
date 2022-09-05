0bec3f302d8fbc6dd0c4277f5ea3744f3ac70429
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-09-05 18:43:07 -0300

    adding process.env.DATABASE_URL

--- api/database.js
+++ api/database.js
@@ -1,8 +1,19 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
-const connection = new Sequelize('dcrqmbbc8bp385','mdiwitafmsyjgh','66c5e11b0e8b9c41a0ad4376f3a7479444c80385b265fbd522cd631aac2061c2', {
-    host: 'ec2-54-204-241-136.compute-1.amazonaws.com',
-    dialect: 'postgres'
+const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
 })
-
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 module.exports = connection
