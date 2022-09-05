668f1f7b655384ba0abee065052d75a444fc6792
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-09-05 18:45:37 -0300

    adding process.env

--- api/database/database.js
+++ api/database/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
-const connection = new Sequelize('gopresence','postgres','147258', {
-    host: 'localhost',
-    dialect: 'postgres'
+const connection = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
 })
+Sequelize.authenticate().then(() => {console.log('Connection has been established successfully.'); }).catch(err => { console.error('Unable to connect to the database:', err);});
 
 module.exports = connection
