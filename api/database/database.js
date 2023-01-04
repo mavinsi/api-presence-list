7e7b68ec1039a108fde67c56adf5fdb63bec0429
Marcus Rodrigues <markynrodrisilva@gmail.com>
2023-01-04 08:39:07 -0300

    changing to railway

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
-const connection  = new Sequelize('postgres://markyn:4dW604oeTIXMkPQzfq3L0APohXPqAuEh@dpg-ceqldd9a6gdovo1319rg-a/dbpresence')
+const connection  = new Sequelize('postgresql://postgres:mSFfyGVGJDeIJFRa3vVT@containers-us-west-144.railway.app:7885/railway')
 //  const connection = new Sequelize('gopresence','postgres','147258', {
     //    host: 'localhost',
     //    dialect: 'postgres'
