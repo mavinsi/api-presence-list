4e9d22a9085d8ebc77457055604c6d6fac818f3c
Marcus Rodrigues <markynrodrisilva@gmail.com>
2023-01-04 07:47:34 -0300

    funciona pelo amor de deus

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
- const connection = new Sequelize('postgres://markyn:4dW604oeTIXMkPQzfq3L0APohXPqAuEh@dpg-ceqldd9a6gdovo1319rg-a.oregon-postgres.render.com/dbpresence')
+const connection  = new Sequelize('postgres://markyn:4dW604oeTIXMkPQzfq3L0APohXPqAuEh@dpg-ceqldd9a6gdovo1319rg-a/dbpresence')
 // const connection = new Sequelize(process.env.DATABASE_URL, {
 //     dialectOptions: {
 //         ssl: {
