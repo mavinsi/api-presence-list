94d61b68d52205ed53888b9a0190db3e5ebe6ca1
Marcus Rodrigues <markynrodrisilva@gmail.com>
2023-02-24 13:40:24 -0300

    Update database.js

--- api/database/database.js
+++ api/database/database.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
-const connection  = new Sequelize('postgresql://postgres:2KQlHJzJald3WYxHptvs@containers-us-west-27.railway.app:6066/railway')
-// const connection = new Sequelize('riseway', 'postgres', '147258', {
-//     host: 'localhost',
-//     dialect: 'postgres'
+// const connection  = new Sequelize('postgresql://postgres:2KQlHJzJald3WYxHptvs@containers-us-west-27.railway.app:6066/railway')
+ const connection = new Sequelize('riseway', 'postgres', '147258', {
+     host: 'localhost',
+     dialect: 'postgres'
 
-// })
+ })
 
 module.exports = connection
