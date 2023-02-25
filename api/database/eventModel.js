56f1225de221d00a41be4204ae45984416c3857f
Marcus Rodrigues <markynrodrisilva@gmail.com>
2023-02-24 22:14:56 -0300

    fixing bugs

--- api/database/eventModel.js
+++ api/database/eventModel.js
@@ -12,7 +12,7 @@ const Event = connection.define('events', {
     },
     eventdesc: {
         type: Sequelize.TEXT,
-        allowNull: true
+        allowNull: false
     },
     eventname: {
         type: Sequelize.TEXT,
