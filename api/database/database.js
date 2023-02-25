fa4181c84a99783b514b0ddfe0c68c97af2b3302
Marcus Rodrigues <markynrodrisilva@gmail.com>
2023-02-24 22:11:23 -0300

    changin database name

--- api/database/database.js
+++ api/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const pg = require('pg')
 // const connection  = new Sequelize('postgresql://postgres:2KQlHJzJald3WYxHptvs@containers-us-west-27.railway.app:6066/railway')
- const connection = new Sequelize('riseway', 'postgres', '147258', {
+ const connection = new Sequelize('gopresence', 'postgres', '147258', {
      host: 'localhost',
      dialect: 'postgres'
 
